7e2dc53e83b771e0333d3e899ab3016ee3b234de
majorek31 <m4y0r.offical@gmail.com>
2022-08-23 23:11:30 +0200

    Fix: map size change 9 -> 8

--- Game/Map.cpp
+++ Game/Map.cpp
@@ -3,7 +3,7 @@ BaseObject* Map::GetObjectAt(int point) {
 	return &this->map[point];
 }
 bool Map::HasFreeField() {
-	for (int i = 0; i < 9; i++) {
+	for (int i = 0; i < 8; i++) {
 		if (this->map[i].GetObjectType() == ObjectType::OBJECT_NULL)
 			return true;
 	}
