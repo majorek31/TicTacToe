54a567b4e9a84c6928418b7b67db4f8fd744b511
majorek31 <m4y0r.offical@gmail.com>
2022-08-24 13:19:24 +0200

    Simple entry to check game api

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,6 +6,11 @@ int main()
     info.aiLevel = AILevel::AI_LEVEL_MODE_EASY;
     info.playerObject = ObjectType::OBJECT_CROSS;
     Game* game = new Game(info);
-    game->GetGameMap()->GetObjectAt(0)->SetObjectType(ObjectType::OBJECT_CROSS);
-    printf_s("%d", game->GetGameMap()->GetObjectAt(0)->GetObjectType());
+    game->GetGameMap()->GetObjectAt(AI::PredictNextBotMove(game, ObjectType::OBJECT_CIRCLE, AILevel::AI_LEVEL_MODE_EASY))->SetObjectType(ObjectType::OBJECT_CIRCLE);
+    ConsoleRenderer::DrawBoard(game);
+    for (size_t i = 0; i < 9; i++)
+    {
+        //printf("%c ", Utils::ConvectObjectTypeToChar(game->GetGameMap()->GetObjectAt(i)->GetObjectType()));
+        //printf("%d\n", i);
+    }
 }
