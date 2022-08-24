6d2c74c9ee32e85c1737b7af94d43ef68bafff9a
majorek31 <m4y0r.offical@gmail.com>
2022-08-24 21:00:28 +0200

    Move rand call into loop

--- Game/AI/AI.cpp
+++ Game/AI/AI.cpp
@@ -4,9 +4,9 @@ int AI::PredictNextBotMove(Game* game, ObjectType botObject, AILevel level) {
 	{
 	case AILevel::AI_LEVEL_MODE_EASY: {
 		int attemptPlace;
-		attemptPlace = rand() % 9;
 		while (game->GetGameMap()->HasFreeField())
 		{
+			attemptPlace = rand() % 9;
 			if (game->GetGameMap()->GetObjectAt(attemptPlace)->GetObjectType() == ObjectType::OBJECT_NULL) {
 				return attemptPlace;
 			}
