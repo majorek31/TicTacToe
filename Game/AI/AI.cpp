b546a88f3eab034ca33ee36ae1dea7b9aa3b46ed
majorek31 <m4y0r.offical@gmail.com>
2022-08-24 03:00:05 +0200

    Fix: for range adjusted to 9

--- Game/AI/AI.cpp
+++ Game/AI/AI.cpp
@@ -4,7 +4,7 @@ int AI::PredictNextBotMove(Game* game, ObjectType botObject, AILevel level) {
 	{
 	case AILevel::AI_LEVEL_MODE_EASY: {
 		int attemptPlace;
-		attemptPlace = rand() % 8;
+		attemptPlace = rand() % 9;
 		while (game->GetGameMap()->HasFreeField())
 		{
 			if (game->GetGameMap()->GetObjectAt(attemptPlace)->GetObjectType() == ObjectType::OBJECT_NULL) {
