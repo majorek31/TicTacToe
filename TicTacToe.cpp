d4df70c3d6691cfb4a9879fb44acb658e286531a
majorek31 <m4y0r.offical@gmail.com>
2022-08-24 20:55:25 +0200

    Complete the basic game engine

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,4 +1,8 @@
-﻿#include <stdio.h>
+﻿#if _WIN32
+#include <Windows.h>
+#endif // _WIN32
+
+#include <stdio.h>
 #include "Game/Game.h"
 int main()
 {
@@ -6,8 +10,25 @@ int main()
     info.aiLevel = AILevel::AI_LEVEL_MODE_EASY;
     info.playerObject = ObjectType::OBJECT_CROSS;
     Game* game = new Game(info);
-    game->GetGameMap()->GetObjectAt(AI::PredictNextBotMove(game, ObjectType::OBJECT_CIRCLE, AILevel::AI_LEVEL_MODE_EASY))->SetObjectType(ObjectType::OBJECT_CIRCLE);
-    ConsoleRenderer::DrawBoard(game);
+    
+    while (true)
+    {
+        #if _WIN32
+            system("cls");
+        #endif // _WIN32 // TODO: do it in correct way instead of calling system.
+        int pnt;
+        ConsoleRenderer::DrawBoard(game);
+        printf("\nIt's your turn: "); scanf_s("%d", &pnt);
+        pnt--;
+        if (game->GetGameMap()->IsFree(pnt)) {
+            game->GetGameMap()->GetObjectAt(pnt)->SetObjectType(info.playerObject);
+            if (game->GetGameMap()->HasFreeField())
+                game->GetGameMap()->GetObjectAt(AI::PredictNextBotMove(game, ObjectType::OBJECT_CIRCLE, info.aiLevel))->SetObjectType(ObjectType::OBJECT_CIRCLE);
+        } else {
+            printf("\nThis is already taken by %c", Utils::ConvectObjectTypeToChar(game->GetGameMap()->GetObjectAt(pnt)->GetObjectType()));
+            Sleep(600);
+        }
+    }
     for (size_t i = 0; i < 9; i++)
     {
         //printf("%c ", Utils::ConvectObjectTypeToChar(game->GetGameMap()->GetObjectAt(i)->GetObjectType()));
