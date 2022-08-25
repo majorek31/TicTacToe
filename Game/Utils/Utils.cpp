62f8090a14c4a4202c8df2504d99462980052d97
majorek31 <m4y0r.offical@gmail.com>
2022-08-25 18:35:26 +0200

    Add CheckForGameState

--- Game/Utils/Utils.cpp
+++ Game/Utils/Utils.cpp
@@ -20,3 +20,30 @@ char Utils::GetCharAtPoint(Game* game, int point) {
 		return point + 0x31; // 0x31 is offset of '1' in ascii table so we use it to convert point to char.
 	return Utils::ConvectObjectTypeToChar(game->GetGameMap()->GetObjectAt(point)->GetObjectType());
 }
+GameState Utils::CheckForGameState(Game* game) {
+	ObjectType currentCheckObject = ObjectType::OBJECT_CIRCLE;
+	for (int i = 0; i < 2; i++) {
+		// horizontal
+		if (game->GetGameMap()->GetObjectAt(0)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(1)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(2)->GetObjectType() == currentCheckObject)
+			return currentCheckObject == ObjectType::OBJECT_CIRCLE ? GameState::STATE_O_WIN : GameState::STATE_X_WIN;
+		if (game->GetGameMap()->GetObjectAt(3)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(4)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(5)->GetObjectType() == currentCheckObject)
+			return currentCheckObject == ObjectType::OBJECT_CIRCLE ? GameState::STATE_O_WIN : GameState::STATE_X_WIN;
+		if (game->GetGameMap()->GetObjectAt(6)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(7)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(8)->GetObjectType() == currentCheckObject)
+			return currentCheckObject == ObjectType::OBJECT_CIRCLE ? GameState::STATE_O_WIN : GameState::STATE_X_WIN;
+		// vertical
+		if (game->GetGameMap()->GetObjectAt(0)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(3)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(6)->GetObjectType() == currentCheckObject)
+			return currentCheckObject == ObjectType::OBJECT_CIRCLE ? GameState::STATE_O_WIN : GameState::STATE_X_WIN;
+		if (game->GetGameMap()->GetObjectAt(1)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(4)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(7)->GetObjectType() == currentCheckObject)
+			return currentCheckObject == ObjectType::OBJECT_CIRCLE ? GameState::STATE_O_WIN : GameState::STATE_X_WIN;
+		if (game->GetGameMap()->GetObjectAt(2)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(5)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(8)->GetObjectType() == currentCheckObject)
+			return currentCheckObject == ObjectType::OBJECT_CIRCLE ? GameState::STATE_O_WIN : GameState::STATE_X_WIN;
+		// diagonally
+		if (game->GetGameMap()->GetObjectAt(0)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(4)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(8)->GetObjectType() == currentCheckObject)
+			return currentCheckObject == ObjectType::OBJECT_CIRCLE ? GameState::STATE_O_WIN : GameState::STATE_X_WIN;
+		if (game->GetGameMap()->GetObjectAt(2)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(4)->GetObjectType() == currentCheckObject && game->GetGameMap()->GetObjectAt(6)->GetObjectType() == currentCheckObject)
+			return currentCheckObject == ObjectType::OBJECT_CIRCLE ? GameState::STATE_O_WIN : GameState::STATE_X_WIN;
+
+		currentCheckObject = ObjectType::OBJECT_CROSS;
+	}
+	return GameState::STATE_RUNNING;
+}
--- Game/Utils/Utils.h
+++ Game/Utils/Utils.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "../Game.h"
+#include "../Types/GameState.h"
 #include "../Types/ObjectType.h"
 class Game;
 namespace Utils {
 	char ConvectObjectTypeToChar(ObjectType type);
 	char GetCharAtPoint(Game* game, int point);
+	GameState CheckForGameState(Game* game);
 }
